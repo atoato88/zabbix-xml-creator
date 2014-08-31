48c2161067a0f3c0c75b3866aaa1208497885785
atoato88 <atoato88@example.com>
2014-08-31 12:23:30 +0900

    replace from xml.etree to jinja2 and csv.

--- zabbix_xml_creator/zabbix_xml_creator.py
+++ zabbix_xml_creator/zabbix_xml_creator.py
@@ -1,7 +1,20 @@
 def load_hosts(path='resource/hosts.csv'):
-    pass
+    def _create_hash(cols, values):
+        result={}
+        for i, c in enumerate(cols):
+            result[c.strip()]=values[i].strip()
+        return result
+    import csv
+    reader = csv.reader(open(path, 'r'))
+    results = []
+    columns = []
+    header = reader.next()
+    for r in reader:
+        results.append(_create_hash(header, r))
+    print results
+    return results
 
-def replace_xml(path='resource', hosts=[{'name':'hogehoge'},{'name':'hogehoge2'}] ):
+def replace_xml(path='resource', hosts=[] ):
     from jinja2 import Environment
     from jinja2.loaders import FileSystemLoader
 
@@ -10,7 +23,7 @@ def replace_xml(path='resource', hosts=[{'name':'hogehoge'},{'name':'hogehoge2'}
     print tmpl.render(hosts=hosts)
 
 def main():
-  replace_xml()
+  replace_xml(hosts=load_hosts())
 
 if __name__ == '__main__':
     main()
